49e614626a4b5c3ef4dd240060ffb3c608baf345
nicolejzhang <43916876+nicolejzhang@users.noreply.github.com>
2021-04-25 21:06:23 -0500

    Update btree.cpp

--- src/btree.cpp
+++ src/btree.cpp
@@ -15,6 +15,7 @@
 #include "exceptions/index_scan_completed_exception.h"
 #include "exceptions/file_not_found_exception.h"
 #include "exceptions/end_of_file_exception.h"
+#include "exceptions/file_exists_exception.h"
 
 
 //#define DEBUG
@@ -33,6 +34,67 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 		const Datatype attrType)
 {
     // Add your code below. Please do not remove this line.
+	
+	// determine the specified index file name
+	std::ostringstream idxStr;
+	idxStr << relationName << '.' << attrByteOffset;
+	outIndexName = idxStr.str();
+
+	// initialization
+	this->bufMgr = bufMgrIn;
+	this->attributeType = attributeType;
+	this->attrByteOffset = attrByteOffset;
+	this->scanExecuting = false;	
+
+	try {
+		// check if the specified index file exist
+		file = new BlobFile(outIndexName, true);
+		
+		// specified index file did not exist
+		// and is created successfully
+		this->headerPageNum = file->getFirstPageNo();
+		Page * metaPage;
+		bufMgr->readPage(file, headerPageNum, metaPage);
+		IndexMetaInfo * metaInfo = (IndexMetaInfo *) metaPage;
+		metaInfo->attrType = attrType;
+		metaInfo->attrByteOffset = attrByteOffset;
+		strcpy(metaInfo->relationName, relationName.c_str());
+
+		// set index file root page number
+		this->rootPageNum = metaInfo->rootPageNo;
+		bufMgr->unPinPage(file, headerPageNum, false);
+
+		// create a root page
+		Page * rootPage;
+		bufMgr->allocPage(file, rootPageNum, rootPage);
+		bufMgr->unPinPage(file, rootPageNum, true);
+
+		// scan the relation
+		FileScan * fscan = new FileScan(relationName, bufMgr);
+		try {
+			RecordId rid;
+			while (true) {
+				fscan->scanNext(rid);
+				
+				// scan record
+				std::string rstr = fscan->getRecord();
+				const char * record = rstr.c_str();
+
+				// scan and construct key
+				int key = *((int *)(record + attrByteOffset));
+				insertEntry(&key, rid);
+			}
+		}
+		catch (EndOfFileException e) {
+			bufMgr->flushFile(file);
+		}
+
+		// erase the fscan object
+		delete fscan;
+	} catch (FileExistsException & e) {
+		// open the index file - TODO
+	}	
+
 }
 
 
@@ -43,6 +105,9 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 BTreeIndex::~BTreeIndex()
 {
     // Add your code below. Please do not remove this line.
+	bufMgr->clearBufStats();
+	bufMgr->flushFile(file);
+	delete file;
 }
 
 // -----------------------------------------------------------------------------
@@ -82,6 +147,10 @@ void BTreeIndex::scanNext(RecordId& outRid)
 void BTreeIndex::endScan() 
 {
     // Add your code below. Please do not remove this line.
+	if (!scanExecuting) 
+		throw ScanNotInitializedException();
+	scanExecuting = false;
+	bufMgr->unPinPage(file, currentPageNum, false);
 }
 
 }
